95619b457518c015e2af294e9809af65ded42186
Sydni <will.sydni@gmail.com>
2020-06-19 12:17:50 -0700

    Added additional API fetch

--- src/components/App.js
+++ src/components/App.js
@@ -5,6 +5,10 @@ class App extends Component {
     super(props);
     this.state = {
       films: [],
+      vehicles: [],
+      isLoaded: false,
+      showFilms: false,
+      hasVehicle: false
     }
   }
 
@@ -13,35 +17,81 @@ class App extends Component {
       .then((res) => res.json())
       .then((films) => this.setState({ films }))
 
+      fetch("https://ghibliapi.herokuapp.com/vehicles")
+      .then((res) => res.json())
+      .then((vehicles) => this.setState({ vehicles }))
+
+  }
+
+
+
+  toggleFilms = () => {
+    this.setState({
+      isLoaded: true,
+      showFilms: true
+    })
+  }
+
+  toggleVehicles = () => {
+    this.setState({
+      isLoaded: true,
+      hasVehicle: true
+    })
   }
 
   render() {
-
-
-    return (
-
-      this.state.films.map((film) => {
+      if(this.state.isLoaded){
+        if(this.state.showFilms){
         return (
+          this.state.films.map((film) => {
+            return (
+    
+              <div key={film.id} className="card" style={{ width: "18rem" }}>
+                <div className="card-body">
+                  <h5 className="card-title">{film.title}</h5>
+                  <h6 className="card-subtitle mb-2 text-muted">{film.director}</h6>
+                  <p className="card-text">{film.description}</p>
+                  <a href={film.url} target="_blank" className="card-link">Go to film</a>
+                </div>
+              </div>
+    
+    
+            )
+          })
+        )}else if (this.state.hasVehicle){
+          return (
+            this.state.vehicles.map((vehicle) => {
+              return (
+      
+                <div key={vehicle.id} className="card" style={{ width: "18rem" }}>
+                  <div className="card-body">
+                    <h5 className="card-title">{vehicle.name}</h5>
+                    <h6 className="card-subtitle mb-2 text-muted">{vehicle.vehicle_class}</h6>
+                    <p className="card-text">{vehicle.description}</p>
+                    <a href={vehicle.url} target="_blank" className="card-link">Go to Vehicle</a>
+                  </div>
+                </div>
+      
+      
+              )
+            })
+          )
+        }
+      } else {
+        return (
+          <>
+          <button onClick={this.toggleFilms}   className="btn btn-success">Show Films</button>
+          <button onClick={this.toggleVehicles}   className="btn btn-success">Show Vehicles</button>
+          
 
-          <div key={film.id} className="card" style={{ width: "18rem" }}>
-            <div className="card-body">
-              <h5 className="card-title">{film.title}</h5>
-              <h6 className="card-subtitle mb-2 text-muted">{film.director}</h6>
-              <p className="card-text">{film.description}</p>
-              <a href={film.url} className="card-link">Card link</a>
-            </div>
-          </div>
 
-
+          </>
         )
-      })
-
-
-    )
-
-
+      }
 
   }
+
+
 
 
 }
